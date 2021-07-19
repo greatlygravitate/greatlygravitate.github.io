44822ed65ab0cc0cd568bc81dc94f20a8e6a20d8
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-07-19 18:47:39 -0400

    Update create.js

--- create.js
+++ create.js
@@ -1,1 +1,27 @@
+function create() {
+  var name = getElementById("name").value;
+  var content = getElementById("text").value;
+  var data = { 
+    name: "user";
+    content: "content";
+  };
+  data.name = name;
+  data.content = content;
+  console.log(data);
+  
+  /* fetch('https://greatlygravitate.github.io/posts.json', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success: ', data);
+  })
+  .catch((error) => {
+    console.error('Error: ', error);
+  });
 
+} */
