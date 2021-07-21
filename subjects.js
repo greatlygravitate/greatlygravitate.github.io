a3ae0af528fafe2c48af1d993b4a7570975de87b
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-07-21 17:36:34 -0400

    Update subjects.js

--- subjects.js
+++ subjects.js
@@ -1,3 +1,7 @@
 function space() {
   window.location.replace('https://greatlygravitate.github.io/space');
 }
+
+function atom() {
+  window.location.replace('https://greatlygravitate.github.io/atom');
+}
